95923680fbc94d2965933d9ca315dffa3c389afd
Ezik143 <jakesucgang90@gmail.com>
2025-08-07 14:47:58 +0800

    my myfirst

---
+++ BookClass/Class1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookClass
+{
+    public class Book
+    {
+        private string title;
+        private string author;
+        private int yearPublished;
+        private string genre;
+
+
+        public Book()
+        {
+            title = "Unknown";
+            author = "Unknown";
+            yearPublished = 0;
+            genre = "Unknown";
+        }
+
+        public Book(string title_)
+        {
+            title = title_;
+            author = "Unknown";
+            yearPublished = 0;
+            genre = "Unknown";
+        }
+
+        public Book(string title_, string author_)
+        {
+            title = title_;
+            author = author_;
+            yearPublished = 0;
+            genre = "Unknown";
+        }
+
+        public Book(string title_, string author_, int yearPublished_)
+        {
+            title = title_;
+            author = author_;
+            yearPublished = yearPublished_;
+            genre = "Unknown";
+        }
+        public Book(string title_, string author_, int yearPublished_, string genre_)
+        {
+            title = title_;
+            author = author_;
+            yearPublished = yearPublished_;
+            genre = genre_;
+        }
+
+        public void displayInfo()
+        {
+            Console.WriteLine("Title: " + title);
+            Console.WriteLine("Author: " + author);
+            Console.WriteLine("Year Published: " + yearPublished);
+            Console.WriteLine("Genre: " + genre);
+            Console.WriteLine("-----------------------------");
+        }
+    }
+}
---
+++ Consumer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookClass;
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("test");
+            book1.displayInfo();
+            Book book2 = new Book("test2", "author2");
+            book2.displayInfo();
+            Book book3 = new Book("test3", "author3", 2023);
+            book3.displayInfo();
+            Book book4 = new Book("test4", "author4", 2023, "genre4");
+            book4.displayInfo();
+        }
+    }
+}
